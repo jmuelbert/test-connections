2016cb2567635278fccf6a7296f631a5e8d6352a
Jürgen <juergen.muelbert@gmail.com>
2023-05-08 12:41:33 +0200

    Update GitHub actions

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+"""Sphinx configuration."""
+project = "test-connections"
+author = "Jürgen Mülbert"
+copyright = "2023, Jürgen Mülbert"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_click",
+    "myst_parser",
+]
+autodoc_typehints = "description"
+html_theme = "furo"
